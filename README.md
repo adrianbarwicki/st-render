ff6a5cf84c2c4fd69f6c6ec8a082ae7659a30cc4
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-04-07 18:04:07 +0200

    readme added

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # st-render
 Renders html files with layout
+
+st-render is used at VQ-Labs.com for filling out email templates with dynamic data.
+
+```
+// paths for templates and general layout
+var templateDir = __dirname + "/my-path/templates/";
+var layoutPath = __dirname + "/my-path/email-templates/layout.ejs";
+
+// creates a render function
+var renderer = stRender(templateDir, layoutPath);
+
+// gets a specific template and fills it with data
+var emailHTML = renderer("new-request",{
+    sampleData: {
+        hello: "world"
+    }
+});
+```
+
+# Licence
+MIT
--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 var ejs = require("ejs");
 var fs = require("fs");
 
-if(module.parent){
+if (module.parent) {
     module.exports = RendererFactory;
-}else{
+} else {
    var bodyPath = process.argv[2];
-   var renderer = RendererFactory("./data/","./views/layout.ejs");
+   var renderer = RendererFactory("./data/", "./views/layout.ejs");
    var compiled = renderer(bodyPath);
-   writeToDirectory(compiled,"./compiled",bodyPath);
-   process.exit(-1);
-    
-}
+   
+   writeToDirectory(compiled, "./compiled", bodyPath);
 
+   process.exit(-1);
+}
     
-function RendererFactory(templateDir,layoutPath){
-    
+function RendererFactory(templateDir, layoutPath) {
     return execute;
     
-    function execute(bodyPath,bodyFill){
+    function execute(bodyPath,bodyFill) {
         console.time("[st.render]");
         var layout = getLayout(layoutPath);
         var body =  getBody(bodyPath);
@@ -26,31 +25,27 @@ function RendererFactory(templateDir,layoutPath){
         console.timeEnd("[st.render]");
         return compiled;
     }
-    function getLayout(layoutPath){
+    function getLayout(layoutPath) {
         console.log(`[st.render]: Getting layout from ${layoutPath}`);
         var layout = fs.readFileSync(layoutPath,'utf8');
         return layout;
     }
 
-    function getBody(bodyPath){
+    function getBody(bodyPath) {
         console.log(`[st.render]: Getting body from ${bodyPath}`);
         var body = fs.readFileSync(templateDir+"/"+bodyPath+".ejs",'utf8');
         return body;
     }
 
-    function render(body,layout){
-        var compiled = ejs.render(layout,{body : body});
+    function render(body, layout, defaultData) {
+        var compiled = ejs.render(layout, { body : Object.extend(body, defaultData) });
+
         return compiled;
     }  
 }    
     
-    
- function writeToDirectory(compiled,directory,name){
+ function writeToDirectory (compiled, directory, name) {
         var writePath = directory+"/"+name+".html";
         fs.writeFileSync(writePath, compiled);
         return writePath;
 }    
-
-
-
-
